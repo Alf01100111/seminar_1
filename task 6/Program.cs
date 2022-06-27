109d418804876e82afafeb3312a97cef47a91ae5
Андрей хухарев <alf.master.mail@gmail.com>
2022-06-27 20:54:27 +0300

    task 6

---
+++ task 6/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Ведите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0)
+{
+    System.Console.WriteLine("Чётное");
+}
+else System.Console.WriteLine("Нечетное");   
