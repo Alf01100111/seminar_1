b9c33341feefc5120a5b04524d7dbacf6e51a142
Андрей хухарев <alf.master.mail@gmail.com>
2022-06-27 21:09:01 +0300

    task 8

---
+++ task 8/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Ведите число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+
+while (index <= num1)
+{
+    System.Console.WriteLine(index);
+    index += 2;
+}
