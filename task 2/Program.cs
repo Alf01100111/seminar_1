ec006989d066ec0b6bce5f986df94a72f8fb53e3
Андрей хухарев <alf.master.mail@gmail.com>
2022-06-27 20:47:12 +0300

    task 4

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -1,19 +1,19 @@
 ﻿System.Console.Write("Ведите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите второе число: ");
+System.Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num1 > num2)
 {
     System.Console.Write(num1);
-    System.Console.WriteLine("Первое число больше");
+    System.Console.WriteLine(" Первое число больше");
 }
 else if (num2 > num1)
 {
     System.Console.Write(num2);
-    System.Console.WriteLine("Второе число больше");
+    System.Console.WriteLine(" Второе число больше");
 }
 else
 {
-    System.Console.WriteLine("Числа равны");
+    System.Console.WriteLine(" Числа равны");
 }
